6b5f05e26a204c36e00fa5762079596cbd02adbe
Flavio Marques <flavio.marques@gcbinvestimentos.com>
2022-09-19 22:43:59 -0300

    chore: nestjs app with serverless-http wrapper

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Callback, Context, Handler } from 'aws-lambda';
+import { NestFactory } from '@nestjs/core';
+import * as serverless from 'serverless-http';
+
+import { AppModule } from './app.module';
+
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp);
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
